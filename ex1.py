ca5229d688ce3d239d940244e52be751928029f0
Jussi Tiira <jussi.tiira@helsinki.fi>
2014-04-11 11:05:28 +0300

    ei toimika oikein

--- ex1.py
+++ ex1.py
@@ -11,9 +11,15 @@ from pymiecoated import Mie
 from operator import add
 
 class ParticleRange:
-    def __init__(self,x_min,x_max,m,samples=1000,name=''):
+    def __init__(self,x_min,x_max,m,samples=1000,name='',wavelength=None):
         self.m = m
         self.name = name
+        self.wavelength = wavelength
+        self.use_size = wavelength is not None
+        if self.use_size:
+            x_min = x_min/wavelength
+            x_max = x_max/wavelength
+            name += ', lambda = ' + str(wavelength)
         self.x = 2*np.pi*np.logspace(np.log10(x_min),np.log10(x_max),num=samples)
         self.mie = [Mie(x=x,m=m) for x in self.x]
             
@@ -30,9 +36,9 @@ class ParticleRange:
         for q in ['qext','qsca','qabs','qb']:
             i += 1
             plt.subplot(4,1,i)
-            plt.xlabel('Size parameter')
+            plt.xlabel(self.xlabel())
             plt.ylabel(qstr[q])
-            plt.loglog(self.x,self.params(q),self.x,np.array(getattr(self,'ray_'+q)()))
+            plt.loglog(self.xvalues(),self.params(q),self.x,np.array(getattr(self,'ray_'+q)()))
         plt.suptitle('scattering efficiencies of '+self.name)
         f.show()
         
@@ -40,16 +46,17 @@ class ParticleRange:
         f = plt.figure()
         plt.title('Mie vs. Rayleigh scattering efficiency ratio with ' + self.name)
         plt.ylabel(r'$Q_{Mie}/Q_{Ray}$')
-        plt.xlabel('Size parameter')
-        plt.loglog(self.x,map(add,self.params('qsca'),-self.ray_qsca()))
+        plt.xlabel(self.xlabel())
+        plt.loglog(self.xvalues(),map(add,self.params('qsca'),-self.ray_qsca()))
         f.show()
         
     def plot_error(self):
         f = plt.figure()
         plt.title('Absolute error (' + self.name + ')')
-        plt.xlabel('Size parameter')
-        plt.ylabel(r'$(Q_{Mie}-Q_{Ray}/Q_{Mie}*100%$')
-        plt.loglog(self.x,np.subtract(self.params('qsca'),self.ray_qsca()))
+        plt.xlabel(self.xlabel())
+        plt.ylabel(r'$(Q_{Mie}-Q_{Ray})/Q_{Mie}*100%$')
+        y = 100*np.divide(np.subtract(self.params('qsca'),self.ray_qsca()),self.params('qsca'))
+        plt.semilogx(self.xvalues(),y)
         f.show()
         
     def ray_qsca(self):
@@ -67,16 +74,36 @@ class ParticleRange:
     def alpha(self):
         return (self.m**2-1)/(self.m**2+2)
         
+    def part_size(self):
+        return self.x*self.wavelength/(2*np.pi)
+        
+    def xlabel(self):
+        if self.use_size:
+            return 'Particle size (nm)'
+        return 'Size parameter'
+        
+    def xvalues(self):
+        if self.use_size:
+            return self.part_size()
+        return self.x
 
 mi = complex(1.310990,3.11e-9)
 ma = complex(1.57,0.007)
 
-ice_vis = ParticleRange(1e2/3000,1e3/300,mi,name='ice particles, short wavelenght')
+ice_vis = ParticleRange(1e4/3000,1e6/300,mi,name='ice particles, short wavelenght')
 ice_rad = ParticleRange(1e2/3e8,1e3/1e6,mi,name='ice particles, long wavelenght')
 aer = ParticleRange(10/3e3,5e3/3e2,ma,name='mineral dust / aerosols')
+ice_c = ParticleRange(1e4,1e6,mi,wavelength=532,name='ice particles')
+aer_c = ParticleRange(600,5000,mi,wavelength=532,name='mineral dust / aerosols')
 
 ptypes = [ice_vis,ice_rad,aer]
+cptypes = [ice_c,aer_c]
 
-for ptype in ptypes:
-    ptype.plot_eff()
-    ptype.plot_ratio()
+#for ptype in ptypes:
+#    ptype.plot_eff()
+#    ptype.plot_ratio()
+#    ptype.plot_error()
+    
+#for cptype in cptypes:
+#    cptype.plot_ratio()
+#    cptype.plot_error()
